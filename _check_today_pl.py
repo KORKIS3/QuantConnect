3bddee5b79f1590c76efa8f3cd5e7e1bb5094d7a
KORKIS3 <korkis3@github.com>
2026-05-09 11:39:31 -0400

    Cleanup: removed old debug and analysis scripts

--- _check_today_pl.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv(r'C:\Users\Administrator\Desktop\IB_Live\tracking\YM_tracking_2026-05-08.csv')
-
-print(f'Total bars: {len(df)}')
-print(f'Final P/L: {df["session_pl"].iloc[-1]:.1f} pts')
-print(f'Position: {df["position"].iloc[-1]}')
