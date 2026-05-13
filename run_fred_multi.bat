2081262f2c1395a409b36a3ca8a549c9619daf6a
KORKIS3 <korkis3@github.com>
2026-05-13 09:30:48 -0400

    add working directory to run_fred_multi.bat

--- run_fred_multi.bat
+++ run_fred_multi.bat
@@ -1,4 +1,5 @@
 @echo off
+cd /d C:\Users\Administrator\source\repos\KORKIS3\QuantConnect
 start "Fred - Account 1 (DUO158495) MASTER" cmd /k python InteractiveBrokers.py --port 4002 --client-id 1 --account-id DUO158495 --duration 450
 timeout /t 1 /nobreak
 start "Fred - Account 2 (DUQ921172) MIRROR" cmd /k python _mirror_account.py
